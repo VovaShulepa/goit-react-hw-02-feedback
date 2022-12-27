5f6b4fce1b5cad23370627bd2ebb51684f797e8d
Shulepa Volodymyr <shulepatrader@gmail.com>
2022-12-27 23:46:30 +0200

    add some files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+// import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+// import Statistics from './Statistics/Statistics';
+import { Section } from './Section/Section';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = option => {
+    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Section title="Please leave feedback">
+          {/* <Statistics
+          good={1}
+          neutral={1}
+          bad={1}
+          total={1}
+          positivePercentage={1}
+        /> */}
+        </Section>
+        {/* <Section title="Statistics"> */}
+        {/* <FeedbackOptions options={1} onLeaveFeedback={1}></FeedbackOptions> */}
+        {/* </Section> */}
+      </div>
+    );
+  }
+}
